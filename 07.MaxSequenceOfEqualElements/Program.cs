69c507794a633bb84332403efeb5bd187ceed528
Hristov <hristovvd1@gmail.com>
2023-10-11 14:18:16 +0300

    push

---
+++ 07.MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _07.MaxSequenceOfEqualElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine()
+                .Split();
+
+            int newBestCount = 0;
+            string bestCountSymbols = "";
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int count = 1;
+
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] == array[j])
+                    {
+                        count++;
+                        if (count > newBestCount)
+                        {
+                            newBestCount = count;
+                            bestCountSymbols = array[i] + " ";
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            for (int i = 0; i < newBestCount; i++)
+            {
+                Console.Write(bestCountSymbols);
+            }
+        }
+    }
+}
--- Demo2/Program.cs
+++ Demo2/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            
+            int numbers = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numbers; i++)
+            {
+                for (int j = 0; j < numbers; j++)
+                {
+                    
+                }
+            }
         }
     }
 }
